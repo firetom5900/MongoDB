c27f13a5f77c1d76208ed06920b4e24bdf862263
Tim <tmmtsmith@gmail.com>
2014-05-08 04:24:31 -0500

    JSON site

---
+++ ETL/SiteImport.ps1
@@ -0,0 +1,13 @@
+﻿Function GetJsonPage ($site, $db, $collection)
+{
+    $date = Get-Date -f "yyyyMMddHHmmss"
+    
+    $destination = "D:\MongoDB\bin\Import\file" + $date + ".json"
+    $webGrab = New-Object System.Net.WebClient
+    $webGrab.DownloadFile($site, $destination)
+
+    $cmd = "D:\MongoDB\bin\mongoimport.exe -file $destination -db $db -collection $collection"
+    CMD /C $cmd
+}
+
+GetJsonPage -site "" -db "DB" -collection "CO"
