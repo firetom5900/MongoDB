95b9e4830cb3d9a2686374fe6d88f3574384f923
Tim <tmmtsmith@gmail.com>
2014-07-10 04:55:58 -0500

    Create addhabitbackup.ps1

---
+++ ETL/addhabitbackup.ps1
@@ -0,0 +1,13 @@
+Function AddHabitBackup ($db, $c)
+{
+    $completed = Get-Content "C:\Habits\completed.json"
+    $progress = Get-Content "C:\Habits\progress.json"
+    Add-Type -Path "D:\MongoDB\bin\MongoDB.Bson.dll"
+    Add-Type -Path "D:\MongoDB\bin\MongoDB.Driver.dll"
+
+    $mongodb = [MongoDB.Driver.MongoDatabase]::Create('mongodb://Timothy/' + $db +'?safe=true')
+
+    $collection = $mongodb[$c]
+    #[MongoDB.Bson.BsonDocument] $insertvalue = @{ "_id" = "new ISODate()"; "Completed"= $completed; "Progress"= $progress;}
+    $collection.Insert($insertvalue)
+}
