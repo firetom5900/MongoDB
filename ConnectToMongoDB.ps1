8aaba719610a438e508a34cbe878d1fd56841ce7
Tim <tmmtsmith@gmail.com>
2014-08-27 11:08:24 -0500

    Update ConnectToMongoDB.ps1

--- ConnectToMongoDB.ps1
+++ ConnectToMongoDB.ps1
@@ -1,3 +1,6 @@
-﻿## Use ISE (x86); check execution policy
-Add-Type -Path "D:\MongoDB\bin\MongoDB.Bson.dll"
-Add-Type -Path "D:\MongoDB\bin\MongoDB.Driver.dll"
+## Use ISE (x86); check execution policy
+#Add-Type -Path "D:\MongoDB\bin\MongoDB.Bson.dll"
+#Add-Type -Path "D:\MongoDB\bin\MongoDB.Driver.dll"
+
+## SRVDV004 data = SQLSERVER
+mongod --dbpath 
